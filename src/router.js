e0090677e97fb92f9ca10f15588dfaedf0a82dac
Ryan Morr <rm.morr@gmail.com>
2019-02-01 14:50:01 -0500

    Refactor and add support for optional parameters and wildcards

--- src/router.js
+++ src/router.js
@@ -1,81 +1,85 @@
 /**
- * Common variables
+ * Import utils
  */
-const has = {}.hasOwnProperty;
-const paramRe = /:([^\/.\\]+)/g;
+import { hasOwnProperty, getRouteMatcher, normalizeURLPath } from './util';
 
 /**
- * Create a new 'Router' instance with or
- * without the new operator. Optionally pass
- * an object of routes that map the path to
- * the callback function
+ * Router for dispatching callbacks
+ * based on a URI
  *
- * @constructor
- * @param {Object} routes (optional)
- * @api public
+ * @class PathRouter
+ * @api private
  */
-function Router(routes) {
-    if (!(this instanceof Router)) {
-        return new Router(routes);
-    }
-    this.routes = [];
-    if (routes) {
-        for (const path in routes) {
-            if (has.call(routes, path)) {
-                this.route(path, routes[path]);
+class PathRouter {
+
+    /**
+     * Create a new 'Router' and optionally
+     * provide an object of routes that map
+     * the path to the callback function
+     *
+     * @constructor
+     * @param {Object} routes (optional)
+     * @api public
+     */
+    constructor(routes = null) {
+        this.routes = new Map();
+        if (routes) {
+            for (const path in routes) {
+                if (hasOwnProperty(routes, path)) {
+                    this.route(path, routes[path]);
+                }
             }
         }
+    }
+
+    /**
+     * Add a new route to the router with a path
+     * which may or may not contain embedded
+     * parameters from which to extract from
+     * a matching URL and pass to the provided
+     * callback function
+     *
+     * @param {String} path
+     * @param {Function} callback
+     * @return {PathRouter}
+     * @api public
+     */
+    route(path, callback) {
+        path = normalizeURLPath(path);
+        this.routes.set(getRouteMatcher(path), callback);
+        return this;
+    }
+
+    /**
+     * Test a URL against all the routes and invoke
+     * the callback function of the first matching
+     * route
+     *
+     * @param {String} path
+     * @return {PathRouter}
+     * @api public
+     */
+    dispatch(path) {
+        path = normalizeURLPath(path);
+        for (const [matcher, callback] of this.routes) {
+            const params = matcher(path);
+            if (params) {
+                callback.call(null, params);
+                return this;
+            }
+        }
+        return this;
     }
 }
 
 /**
- * Add a new route to the router with a path
- * which may or may not contain embedded
- * parameters from which to extract from
- * a matching URL and pass to the provided
- * callback function:
+ * Factory function for creating
+ * a `PathRouter` instance
  *
- * Router().route('/foo/:bar', function(bar){
- *
- * });
- *
- * @param {String} path
- * @param {Function} callback
- * @return {Router}
+ * @param {Object} routes (optional)
+ * @return {PathRouter}
  * @api public
  */
-Router.prototype.route = function route(path, callback) {
-    paramRe.lastIndex = 0;
-    let regexp = path + '', match;
-    while ((match = paramRe.exec(path))) {
-        regexp = regexp.replace(match[0], '([^/\\\\]+)');
-    }
-    this.routes.push({regexp: new RegExp('^' + regexp + '$'), callback});
-    return this;
-};
-
-/**
- * Test a URL against all the routes and invoke
- * the callback function of the first matching
- * pattern, passing all if any parameters
- *
- * @param {String} path (optional)
- * @return {Router}
- * @api public
- */
-Router.prototype.dispatch = function dispatch(path = window.location.pathname) {
-    for (let i = 0, len = this.routes.length, route, matches; i < len; i++) {
-        route = this.routes[i];
-        matches = route.regexp.exec(path);
-        if (matches && matches[0]) {
-            route.callback.apply(null, matches.slice(1));
-            return this;
-        }
-    }
-    return this;
-};
-
-/**
- * Export `Router` class
- */
-export default Router;
+export default function Router(routes) {
+    return new PathRouter(routes);
+}
--- test/router.js
+++ test/router.js
@@ -1,53 +1,360 @@
-/* eslint-disable max-len, no-unused-expressions */
-
 import { expect } from 'chai';
 import sinon from 'sinon';
 import Router from '../src/router';
 
-describe('Router', () => {
-    it('should support invocation with and without the new operator', () => {
-        expect(Router()).to.be.instanceof(Router);
-        expect(new Router()).to.be.instanceof(Router);
+describe('router', () => {
+    it('should route root paths', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        expect(router.route('/', callback)).to.equal(router);
+
+        expect(router.dispatch('/')).to.equal(router);
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({});
+
+        router.dispatch('');
+        expect(callback.callCount).to.equal(1);
     });
 
-    it('should invoke callback for matching path', () => {
-        const router = Router();
-        const spy = sinon.spy();
-        router.route('/foo/bar', spy);
+    it('should route single static paths', () => {
+        const callback = sinon.spy();
+
+        const router = Router({'/foo': callback});
+        router.route('/bar/', callback);
+
+        router.dispatch('/foo');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({});
+
+        router.dispatch('/foo/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({});
+
+        router.dispatch('/bar');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({});
+
+        router.dispatch('/bar/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({});
+
+        router.dispatch('');
+        router.dispatch('/');
+        router.dispatch('foo/');
+        router.dispatch('foo');
         router.dispatch('/foo/bar');
-        expect(spy.called).to.equal(true);
+        expect(callback.callCount).to.equal(4);
     });
 
-    it('should pass parameters to the callback for matching path', () => {
+    it('should route multiple static paths', () => {
+        const callback = sinon.spy();
+
         const router = Router();
-        const spy = sinon.spy();
-        router.route('/foo/:bar/baz/:qux', spy);
-        router.dispatch('/foo/123/baz/abc');
-        expect(spy.called).to.equal(true);
-        expect(spy.calledWith('123', 'abc')).to.equal(true);
+        router.route('/foo/bar/baz', callback);
+
+        router.dispatch('/foo/bar/baz');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({});
+
+        router.dispatch('/foo/bar/baz/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({});
+
+
+        router.dispatch('foo/bar/baz');
+        router.dispatch('/foo/bar/baz/qux');
+        router.dispatch('/fo/bar/baz');
+        expect(callback.callCount).to.equal(2);
     });
 
-    it('should support special characters as defined by RFC 3986', () => {
-        const spy = sinon.spy();
-        const router = Router({
-            '/foo/:bar': spy
-        });
+    it('should route paths with a single parameter', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/:foo', callback);
+
+        router.dispatch('/aaa');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({foo: 'aaa'});
+
+        router.dispatch('/bbb/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({foo: 'bbb'});
+
+        router.dispatch('');
+        router.dispatch('root');
+        router.dispatch('root/');
+        expect(callback.callCount).to.equal(2);
+
+        router.route('/root/:foo', callback);
+
+        router.dispatch('/root/aaa');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({foo: 'aaa'});
+
+        router.dispatch('/root/bbb/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({foo: 'bbb'});
+
+        router.dispatch('root/foo');
+        router.dispatch('root/foo/');
+        router.dispatch('/root/foo/bar');
+        expect(callback.callCount).to.equal(4);
+    });
+
+    it('should route paths with multiple parameters', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/root/:foo/:bar', callback);
+
+        router.dispatch('/root/foo/bar');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({foo: 'foo', bar: 'bar'});
+
+        router.dispatch('/root/aaa/bbb/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({foo: 'aaa', bar: 'bbb'});
+
+        router.dispatch('root/foo/bar');
+        router.dispatch('root/foo');
+        router.dispatch('root/foo/');
+        expect(callback.callCount).to.equal(2);
+
+        router.route('/root/:foo/static/:bar', callback);
+
+        router.dispatch('/root/foo/static/bar');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({foo: 'foo', bar: 'bar'});
+
+        router.dispatch('/root/aaa/static/bbb/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({foo: 'aaa', bar: 'bbb'});
+
+        router.dispatch('root/foo/static/bar');
+        router.dispatch('/root/foo/staic/bar');
+        router.dispatch('/root/foo/static/bar/baz');
+        expect(callback.callCount).to.equal(4);
+    });
+
+    it('should route paths with a single optional parameter', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/root/:foo?', callback);
+
+        router.dispatch('/root');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({foo: null});
+
+        router.dispatch('/root/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({foo: null});
+
+        router.dispatch('/root/foo');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({foo: 'foo'});
+
+        router.dispatch('/root/bar/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({foo: 'bar'});
+
+        router.dispatch('root/foo/bar');
+        router.dispatch('root/foo/bar/');
+        router.dispatch('/root/foo/bar');
+        expect(callback.callCount).to.equal(4);
+
+        router.route('/:foo?', callback);
+
+        router.dispatch('/');
+        expect(callback.callCount).to.equal(5);
+        expect(callback.args[4][0]).to.deep.equal({foo: null});
+
+        router.dispatch('/foo');
+        expect(callback.callCount).to.equal(6);
+        expect(callback.args[5][0]).to.deep.equal({foo: 'foo'});
+
+        router.dispatch('/bar/');
+        expect(callback.callCount).to.equal(7);
+        expect(callback.args[6][0]).to.deep.equal({foo: 'bar'});
+
+        router.dispatch('');
+        expect(callback.callCount).to.equal(7);
+    });
+
+    it('should route paths with multiple optional parameters', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/root/:foo?/static/:bar?', callback);
+
+        router.dispatch('/root/foo/static');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({foo: 'foo', bar: null});
+
+        router.dispatch('/root/bar/static/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({foo: 'bar', bar: null});
+
+        router.dispatch('/root/foo/static/bar');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({foo: 'foo', bar: 'bar'});
+
+        router.dispatch('/root/aaa/static/bbb/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({foo: 'aaa', bar: 'bbb'});
+
+        router.dispatch('root/foo/static');
+        router.dispatch('root/foo/static/bar/');
+        router.dispatch('/root/foo/static/bar/baz');
+        router.dispatch('/root/foo/staic/bar');
+        expect(callback.callCount).to.equal(4);
+
+        router.route('/root/:foo?/:bar?', callback);
+
+        router.dispatch('/root/foo');
+        expect(callback.callCount).to.equal(5);
+        expect(callback.args[4][0]).to.deep.equal({foo: 'foo', bar: null});
+
+        router.dispatch('/root/bar/');
+        expect(callback.callCount).to.equal(6);
+        expect(callback.args[5][0]).to.deep.equal({foo: 'bar', bar: null});
+
+        router.dispatch('/root/foo/bar');
+        expect(callback.callCount).to.equal(7);
+        expect(callback.args[6][0]).to.deep.equal({foo: 'foo', bar: 'bar'});
+
+        router.dispatch('/root/aaa/bbb/');
+        expect(callback.callCount).to.equal(8);
+        expect(callback.args[7][0]).to.deep.equal({foo: 'aaa', bar: 'bbb'});
+
+        router.dispatch('root/foo');
+        router.dispatch('root/foo/');
+        router.dispatch('root/foo/bar');
+        router.dispatch('root/foo/bar/');
+        router.dispatch('/root/foo/bar/baz');
+        expect(callback.callCount).to.equal(8);
+    });
+
+    it('should route wildcards', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/root/*', callback);
+
+        router.dispatch('/root/foo');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({wildcard: 'foo'});
+
+        router.dispatch('/root/bar/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({wildcard: 'bar'});
+
+        router.dispatch('/root/foo/bar');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({wildcard: 'foo/bar'});
+
+        router.dispatch('/root');
+        router.dispatch('/root/');
+        router.dispatch('root/foo');
+        router.dispatch('root/foo/');
+        expect(callback.callCount).to.equal(3);
+
+        router.route('/*', callback);
+
+        router.dispatch('/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({wildcard: ''});
+
+        router.dispatch('/foo');
+        expect(callback.callCount).to.equal(5);
+        expect(callback.args[4][0]).to.deep.equal({wildcard: 'foo'});
+
+        router.dispatch('/bar/');
+        expect(callback.callCount).to.equal(6);
+        expect(callback.args[5][0]).to.deep.equal({wildcard: 'bar'});
+
+        router.dispatch('/foo/bar');
+        expect(callback.callCount).to.equal(7);
+        expect(callback.args[6][0]).to.deep.equal({wildcard: 'foo/bar'});
+
+        router.dispatch('');
+        expect(callback.callCount).to.equal(7);
+    });
+
+    it('should route paths with parameters and optional parameters', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/root/:foo/:bar?/:baz?', callback);
+
+        router.dispatch('/root/foo');
+        expect(callback.callCount).to.equal(1);
+        expect(callback.args[0][0]).to.deep.equal({foo: 'foo', bar: null, baz: null});
+
+        router.dispatch('/root/bar/');
+        expect(callback.callCount).to.equal(2);
+        expect(callback.args[1][0]).to.deep.equal({foo: 'bar', bar: null, baz: null});
+
+        router.dispatch('/root/foo/bar');
+        expect(callback.callCount).to.equal(3);
+        expect(callback.args[2][0]).to.deep.equal({foo: 'foo', bar: 'bar', baz: null});
+
+        router.dispatch('/root/aaa/bbb/');
+        expect(callback.callCount).to.equal(4);
+        expect(callback.args[3][0]).to.deep.equal({foo: 'aaa', bar: 'bbb', baz: null});
+
+        router.dispatch('/root/foo/bar/baz');
+        expect(callback.callCount).to.equal(5);
+        expect(callback.args[4][0]).to.deep.equal({foo: 'foo', bar: 'bar', baz: 'baz'});
+
+        router.dispatch('/root/aaa/bbb/ccc/');
+        expect(callback.callCount).to.equal(6);
+        expect(callback.args[5][0]).to.deep.equal({foo: 'aaa', bar: 'bbb', baz: 'ccc'});
+
+        router.dispatch('/root');
+        router.dispatch('/root/');
+        router.dispatch('root/foo');
+        expect(callback.callCount).to.equal(6);
+    });
+
+    it('should route special characters as defined by RFC 3986', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/foo/:bar', callback);
+
         router.dispatch('/foo/-._~:?#[]@!$&\'()*+,;=');
-        expect(spy.called).to.equal(true);
-        expect(spy.calledWith('-._~:?#[]@!$&\'()*+,;=')).to.equal(true);
+        expect(callback.called).to.equal(true);
+        expect(callback.args[0][0].bar).to.equal('-._~:?#[]@!$&\'()*+,;=');
     });
 
-    it('should stop matching after finding the first successful match', () => {
-        const spy1 = sinon.spy();
-        const spy2 = sinon.spy();
-        const spy3 = sinon.spy();
-        Router()
-            .route('/foo', spy1)
-            .route('/bar', spy2)
-            .route('/baz', spy3)
-            .dispatch('/foo');
-        expect(spy1.called).to.equal(true);
-        expect(spy2.called).to.equal(false);
-        expect(spy3.called).to.equal(false);
+    it('should decode URL-encoded parameters', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/foo/:bar', callback);
+
+        router.dispatch('/foo/%D1%88%D0%B5%D0%BB%D0%BB%D1%8B');
+        expect(callback.called).to.equal(true);
+        expect(callback.args[0][0].bar).to.equal('шеллы');
+    });
+
+    it('should convert string versions of a primitive value to its natural type', () => {
+        const callback = sinon.spy();
+
+        const router = Router();
+        router.route('/:string/:true/:false/:int/:float', callback);
+
+        router.dispatch('/foo/true/false/123/45.891');
+        expect(callback.called).to.equal(true);
+
+        const params = callback.args[0][0];
+        expect(params.string).to.equal('foo');
+        expect(params.true).to.equal(true);
+        expect(params.false).to.equal(false);
+        expect(params.int).to.equal(123);
+        expect(params.float).to.equal(45.891);
     });
 });
