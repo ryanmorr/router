7165fa34c6a9ddcbb227e0b46a10b59974580055
Ryan Morr <rm.morr@gmail.com>
2023-03-26 14:57:17 -0400

    Refactor

--- src/router.js
+++ src/router.js
@@ -1,64 +1,85 @@
-/**
- * Import utils
- */
-import { hasOwnProperty, getRouteMatcher, normalizeURLPath } from './util.js';
+const TRAILING_SLASH_RE = /\/$/;
 
-/**
- * Router for dispatching callbacks
- * based on a URI
- *
- * @class PathRouter
- * @api private
- */
+function isNumeric(value) {
+    return !Number.isNaN(parseFloat(value)) && isFinite(value);
+}
+
+function coerce(value) {
+    if (value === 'true') {
+        return true;
+    }
+    if (value === 'false') {
+        return false;
+    }
+    if (value === 'undefined') {
+        return null;
+    }
+    if (isNumeric(value)) {
+        return Number(value);
+    }
+    return value;
+}
+
+function normalizeURLPath(path) {
+    path = path.trim();
+    return path === '/' ? path : path.replace(TRAILING_SLASH_RE, '');
+}
+
+function getRouteMatcher(path) {
+    const keys = [];
+    const pattern = path.split('/').map((part) => {
+        if (!part) {
+            return part;
+        }
+        const length = part.length;
+        const code = part.charCodeAt(0);
+        if (code === 42) {
+            keys.push('wildcard');
+            return '/(.*)';
+        } else if (code === 58) {
+            const optional = part.charCodeAt(length - 1) === 63;
+            keys.push(part.substring(1, optional ? length - 1 : length));
+            if (optional) {
+                return '(?:/([^/]+?))?';
+            }
+            return '/([^/]+?)';
+        }
+        return '/' + part;
+    });
+    const regex = new RegExp('^' + pattern.join('') + '/?$', 'i');
+    return (path) => {
+        const matches = regex.exec(path);
+        if (matches && matches[0]) {
+            return matches
+                .slice(1)
+                .map(decodeURI)
+                .map(coerce)
+                .reduce((map, value, i) => {
+                    map[keys[i]] = value;
+                    return map;
+                }, {});
+        }
+        return null;
+    };
+}
+
 class PathRouter {
 
-    /**
-     * Create a new 'Router' and optionally
-     * provide an object of routes that map
-     * the path to the callback function
-     *
-     * @constructor
-     * @param {Object} routes (optional)
-     * @api public
-     */
     constructor(routes = null) {
         this.routes = new Map();
         if (routes) {
             for (const path in routes) {
-                if (hasOwnProperty(routes, path)) {
-                    this.route(path, routes[path]);
-                }
+                this.route(path, routes[path]);
             }
         }
     }
 
-    /**
-     * Add a new route to the router with a path
-     * which may or may not contain embedded
-     * parameters from which to extract from
-     * a matching URL and pass to the provided
-     * callback function
-     *
-     * @param {String} path
-     * @param {Function} callback
-     * @return {PathRouter}
-     * @api public
-     */
     route(path, callback) {
         path = normalizeURLPath(path);
         this.routes.set(getRouteMatcher(path), callback);
         return this;
     }
 
-    /**
-     * Test a URL against all the routes and invoke
-     * the callback function of the first matching
-     * route
-     *
-     * @param {String} path
-     * @return {PathRouter}
-     * @api public
-     */
     dispatch(path) {
         path = normalizeURLPath(path);
         for (const [matcher, callback] of this.routes) {
@@ -72,14 +93,6 @@ class PathRouter {
     }
 }
 
-/**
- * Factory function for creating
- * a `PathRouter` instance
- *
- * @param {Object} routes (optional)
- * @return {PathRouter}
- * @api public
- */
 export default function Router(routes) {
     return new PathRouter(routes);
 }
